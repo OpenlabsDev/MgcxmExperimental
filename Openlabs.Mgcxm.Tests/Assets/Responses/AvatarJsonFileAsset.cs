ba38a029a2aee8545db4f96111c2bf6f776cf5ce
nexusisbadateverything@gmail.com <nexusisbadateverything@gmail.com>
2023-08-05 00:52:19 +0100

    commit

--- Openlabs.Mgcxm.Tests/Assets/Responses/AvatarJsonFileAsset.cs
+++
@@ -1,9 +0,0 @@
-// Copr. (c) Nexus 2023. All rights reserved.
-
-using Openlabs.Mgcxm.Assets;
-
-namespace Openlabs.Mgcxm.Tests.Assets.Responses;
-
-public sealed class AvatarJsonFileAsset : DTOJsonFileAsset<DTO.AvatarDTO>
-{
-}
--- Openlabs.Mgcxm.Tests/Assets/Responses/DTO/MessageDTO.cs
+++
@@ -1,8 +0,0 @@
-// Copr. (c) Nexus 2023. All rights reserved.
-
-namespace Openlabs.Mgcxm.Tests.Assets.Responses.DTO;
-
-public class MessageDTO
-{
-    
-}
